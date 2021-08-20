14ea25a9a82aade66fbf664052bc0e88abf8a1a0
Carey Metcalfe <carey@cmetcalfe.ca>
2021-08-19 22:52:36 -0400

    Bump version to v0.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except Exception:
 
 setup(
     name="strava-backup",
-    version="0.2.0",
+    version="0.2.1",
     description="Get your data back from Strava",
     long_description=long_description,
     long_description_content_type="text/markdown",
